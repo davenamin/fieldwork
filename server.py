48d16d7d0eb8c474f081685e3afff141232d787f
davenamin <amind@my.uri.edu>
2017-09-16 17:06:19 -0400

    starting to work on data layer, but needs some more thought

--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 # Daven Amin, 09/15/2017
 import os
 import json
+from datetime import datetime
 from pathlib import Path
 from google.oauth2 import service_account
 from google.auth.transport.requests import AuthorizedSession
@@ -12,6 +13,7 @@ import gspread
 from flask import Flask, render_template, url_for
 from flask_socketio import SocketIO
 
+# ---------------- app config -------------- #
 # set up flask and socketio
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['FLASK_SECRET']
@@ -42,7 +44,45 @@ js_files = [Path(os.path.join(parent.replace('static'+os.sep, '', 1), x))
             for x in files
             if x.endswith(".js")]
 
+# ------------- app startup --------------- #
+ref_worksheet = backend.worksheet("gis_dataset")
+ref_cells = ref_worksheet.range(1, 1,
+                                ref_worksheet.row_count,
+                                ref_worksheet.col_count)
+ref_updated_time = datetime.strptime(ref_worksheet.updated,
+                                     '%Y-%m-%dT%H:%M:%S.%fZ')
 
+# building the app's notion of the data model
+# unfortunately, we don't have any notion of a primary key, so use row nums
+data_model = {key: {} for key in range(ref_worksheet.row_count - 1)}
+data_headers = {cell.col: cell.value for cell in ref_cells if cell.row == 1}
+for cell in ref_cells:
+    if cell.row != 1:  # if not header
+        data_row = cell.row-2
+        data_col = data_headers[cell.col]
+        data_model[data_row][data_col] = (cell, cell.value, None)
+
+
+# ------------- app logic ----------------- #
+def get_updated_records():
+    """checks if the reference worksheet has been updated since last fetched.
+    returns any row numbers which have been added or modified"""
+    cur_updated_time = datetime.strptime(ref_worksheet.updated,
+                                         '%Y-%m-%dT%H:%M:%S.%fZ')
+    if cur_updated_time > ref_updated_time:
+        updated_rows = []
+        for row, row_dict in data_model.items():
+            for col_name, cell_tracker in row_dict:
+                if cell_tracker[0].value != cell_tracker[1]:
+                    cell_tracker[1] = cell_tracker[0].value
+                    cell_tracker[2] = None
+                    updated_rows.append(row)
+        return updated_rows
+    else:
+        return []
+
+
+# ------------ flask routing ------------- #
 @app.route('/')
 def index():
     """main entry point - serves a page which initiates websocket comms"""
