fc11467d3f83417c88da187596a5fbc62a2567f6
davenamin <amind@my.uri.edu>
2017-09-24 09:35:06 -0400

    more hacky changes to frontend

--- static/app_code/map.js
+++ static/app_code/map.js
@@ -24,6 +24,19 @@ map_fn = function () {
     info.onAdd = function (map) {
         this._div = L.DomUtil.create('div', 'status');
         this.updateConnectionStatus("disconnected");
+        this._div.innerHTML.style.cssText = " .info { \
+            padding: 6px 8px; \
+            font: 14px/16px Arial, Helvetica, sans-serif; \
+            background: white; \
+            background: rgba(255,255,255,0.8); \
+            box-shadow: 0 0 15px rgba(0,0,0,0.2); \
+            border-radius: 5px; \
+        } \
+        .info h4 { \
+            margin: 0 0 5px; \
+            color: #777; \
+        } \
+        ";
         return this._div;
     };
     info.updateConnectionStatus = function (status) {
@@ -51,9 +64,32 @@ map_fn = function () {
         for (var i = 0; i < status.length; i++) {
             div.innerHTML +=
                 '<i style="background:' + colors[i] + '"></i> ' +
-                status[i] + (status[i + 1] ? '&ndash;' + status[i + 1] + '<br>' : '+');
+                status[i] + (status[i + 1] ? '<br>' : '');
         }
-
+        div.innerHTML.style.cssText = " .info { \
+            padding: 6px 8px; \
+            font: 14px/16px Arial, Helvetica, sans-serif; \
+            background: white; \
+            background: rgba(255,255,255,0.8); \
+            box-shadow: 0 0 15px rgba(0,0,0,0.2); \
+            border-radius: 5px; \
+        } \
+        .info h4 { \
+            margin: 0 0 5px; \
+            color: #777; \
+        } \
+        .legend { \
+            line-height: 18px; \
+            color: #555; \
+        } \
+        .legend i { \
+            width: 18px; \
+            height: 18px; \
+            float: left; \
+            margin-right: 8px; \
+            opacity: 0.7; \
+        } \
+        ";
         return div;
     };
 
